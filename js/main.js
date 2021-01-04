14227ae7e46169cd8c1a360cc11817bb4bd74a8a
ctrl-nobody <par.12092000@gmail.com>
2021-01-04 23:21:40 +0300

    create: parallax and posts

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(".header__mobile-menu").click(function(){
     $(".mobile-menu-hidden").toggleClass("mobile-menu-visible");
     $("body").toggleClass("body-another");
+    $(".parallax-mirror").toggleClass("parallax-another");
 });
+$('.background__image').parallax({imageSrc: '../img/back-one.jpg', speed: 0.0009, positionY: 'bottom'});
