4bc610846f918741a9995847a23f827bffca7d5c
ctrl-nobody <par.12092000@gmail.com>
2021-01-04 20:10:59 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 $(".header__mobile-menu").click(function(){
     $(".mobile-menu-hidden").toggleClass("mobile-menu-visible");
-    $("body").toggleClass("body-another");
+    $("html").toggleClass("body-another");
 });
